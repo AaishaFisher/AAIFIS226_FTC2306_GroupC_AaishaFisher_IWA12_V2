3ef464ffc539ecdec0303f1f4c96f2a4b411fe93
Aai'sha Fisher <aaishafisher484@gmail.com>
2023-11-28 09:36:33 +0200

    Update selectors to target elements within '#book1' container

--- script.js
+++ script.js
@@ -1,42 +1,45 @@
 const STATUS_MAP = {
     shelf: {
         color: 'green',
-        canReserve: true,
-        canCheckout: true,
-        canCheckIn: false,
+        canReserve: true, //yes
+        canCheckout: true, //yes
+        canCheckIn: false, //no
     },
     reserved: {
         color: 'blue',
-        canReserve: false,
-        canCheckout: true,
-        canCheckIn: false,
+        canReserve: false, //no
+        canCheckout: true, //yes
+        canCheckIn: false, //no
     },
     overdue: {
         color: 'red',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
+        canReserve: false, //no
+        canCheckout: false, //no
+        canCheckIn: true, //yes
     },
     checkedOut: {
         color: 'orange',
-        canReserve: false,
-        canCheckout: false,
-        canCheckIn: true,
+        canReserve: false, //no
+        canCheckout: false, //no
+        canCheckIn: true, //yes
     }
-}
+};
 
 // Edit below line 
 
+//book1
+const status1 = document.querySelector('#book1 .status');
+const reserve1 = document.querySelector('#book1 .reserve');
+const checkout1 = document.querySelector('#book1 .checkout');
+const checkin1 = document.querySelector('#book1 .checkin');
+
+//book2
 status = selector(status)
 reserve = selector(reserve)
 checkout = selector(checkout)
 checkin = selector(checkin)
 
-status = selector(status)
-reserve = selector(reserve)
-checkout = selector(checkout)
-checkin = selector(checkin)
-
+//book3
 status = selector(status)
 reserve = selector(reserve)
 checkout = selector(checkout)
